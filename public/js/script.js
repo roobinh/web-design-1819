1109ab82c20ab36f4b4b60378d357d333f130343
Robin Hofman <roobin1999@gmail.com>
2019-04-15 13:02:35 +0200

    yahoo api

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
 console.log("script.js imported.")
+
+document.onkeydown = function(e) {
+    if(e.keyCode == 37) { // left Key
+        console.log("Left")
+    } else if(e.keyCode == 38) { // up key
+        console.log("Up")
+    } else if(e.keyCode == 39) { //right key
+        console.log("Right")
+    } else if(e.keyCode == 40) { //down key
+        console.log("Down")
+    }
+}
